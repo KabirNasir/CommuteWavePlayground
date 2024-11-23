8d063b0ee8dff735105ffa8cacfbb07e884b626d
I584064 <avish.shetty@sap.com>
2024-11-23 18:09:26 +0530

    Add fiori elements components

---
+++ CAP/app/nodal_points/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("commute.wave.nodalpoints.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ CAP/app/services.cds
@@ -0,0 +1,3 @@
+
+
+using from './nodal_points/annotations';
